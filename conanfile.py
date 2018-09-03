f24478a5142419e305138b02b52bf12e9fc1bb0f
Edgar <edgar@anotherfoxguy.com>
2018-09-03 09:55:35 +0000

    Updated cmake

--- conanfile.py
+++ conanfile.py
@@ -11,18 +11,17 @@ class MofilereaderConan(ConanFile):
     #options = {"shared": [True, False]}
     #default_options = "shared=False"
     generators = "cmake"
+    exports_sources = "include*", "src*", "CMakeLists.txt"
 
-    def source(self):
-        self.run("git clone https://bitbucket.org/scorcher24/mofilereader.git .")
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(source_folder="build")
+        cmake.configure()
         cmake.build()
 
     def package(self):
         self.copy("*.h", dst="include", src="include")
-        self.copy("*.lib", dst="lib", src=os.path.join("..", "lib"), keep_path=False)
+        self.copy("*.lib", dst="lib", keep_path=False)
         self.copy("*.dll", dst="bin", keep_path=False)
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
