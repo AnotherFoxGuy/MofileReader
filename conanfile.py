2518de0610fa49fa703a0cd7d543fb73015fbea8
Edgar <Edgar@AnotherFoxGuy.com>
2021-01-12 13:57:32 +0100

    :wrench: Updated conan package

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,14 @@
-from conans import ConanFile, CMake, tools
-import os
+from conans import ConanFile
+
 
 class MofilereaderConan(ConanFile):
     name = "MofileReader"
-    version = "1.0.0"
+    version = "1.1.0"
     license = "MIT"
-    url = "https://github.com/AnotherFoxGuy/conan-MofileReader/"
+    url = "https://github.com/AnotherFoxGuy/MofileReader/"
     description = "This API lets you read .mo-Files and use their content just as you would do with GNUs gettext."
     settings = "os", "compiler", "build_type", "arch"
-    exports_sources = "include*", "src*", "CMakeLists.txt"
-
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+    exports_sources = "include*"
 
     def package(self):
-        cmake = CMake(self)
-        cmake.install()
-
-    def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.copy("*.hpp", "include", "include")
