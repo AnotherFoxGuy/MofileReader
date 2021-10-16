a254d89311e897b3029a949df58fa398681b1671
Edgar <Edgar@AnotherFoxGuy.com>
2021-10-16 11:50:24 +0200

    :white_check_mark: Added Unicode tests

--- test/extract_pot.sh
+++ test/extract_pot.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xgettext -o test.pot -k_ -k"_LC:1c,2" -s -c --package-name=moFileReader test.cpp
+xgettext -o test.pot -k"_L" -k"_LC:1c,2" -s -c --package-name=moFileReader --from-code=UTF-8 test.cpp
--- test/test.cpp
+++ test/test.cpp
@@ -24,7 +24,7 @@ TEST_CASE("Count number of strings", "[Count]")
 {
     moFileReader moFR;
     moFR.ReadFile(MO_TEST_FILE);
-    CHECK(7 ==  moFR.GetNumStrings());
+    CHECK(11 ==  moFR.GetNumStrings());
 }
 
 TEST_CASE("Empties the Lookup-Table", "[Count]")
@@ -32,7 +32,7 @@ TEST_CASE("Empties the Lookup-Table", "[Count]")
     moFileReader moFR;
     moFR.ReadFile(MO_TEST_FILE);
     CHECK("Text Nederlands Een" == moFR.Lookup("String English One"));
-    CHECK(7 ==  moFR.GetNumStrings());
+    CHECK(11 ==  moFR.GetNumStrings());
     moFR.ClearTable();
     CHECK("String English One" == moFR.Lookup("String English One"));
     CHECK(0 ==  moFR.GetNumStrings());
@@ -48,6 +48,19 @@ TEST_CASE("Lookup string", "[Lookup]")
     CHECK("Text Nederlands Twee" == _L("String English Two"));
     /* This is the third comment.  */
     CHECK("Text Nederlands Drie" == _L("String English Three"));
+}
+
+TEST_CASE("Lookup string with unicode", "[LookupUnicode]")
+{
+    moFileReader moFR;
+    moFR.ReadFile(MO_TEST_FILE);
+    CHECK("Omrop Fryslân maakt radio en televisie in het Fries, daarmee is de omroep de enige zender van Nederland die niet in het Nederlanders uitzendt"
+          == _L("Omrop Fryslân makket radio en telefyzje yn it Frysk, dêrmei is de omrop de ienige stjoerder fan Nederlân dy’t net yn it Nederlânsk útstjoert"));
+    CHECK("De namme ‘Omrop Fryslân’ hâldt yn: regionaal werkenber, aktueel, ûnôfhinklik, betrouber, tagonklik, ûnderhâldend en divers"
+          == _L("De naam 'Omrop Fryslân' houdt in: Regionaal herkenbaar, actueel, onafhankelijk, betrouwbaar, toegankelijk, vermakelijk en divers"));
+    CHECK("Bûter, brea en brûne sûker" == moFR.Lookup("Bûter, brea en brûne sûker"));
+    CHECK("veranderd ß" == _L("geändert ß"));
+    CHECK("Ik 💚 Friesland" == _L("I 💚 Fryslân"));
 }
 
 TEST_CASE("Lookup string with context", "[LookupWithContext]")
@@ -74,6 +87,6 @@ TEST_CASE("Lookup not existing strings with context", "[LookupWithContext-fail]"
 {
     moFileReader moFR;
     moFR.ReadFile(MO_TEST_FILE);
-    CHECK("String English" == _LC("Nope", "String English"));
-    CHECK("Not this one" == _LC("TEST|String|1", "Not this one"));
+    CHECK("String English" == moFR.LookupWithContext("Nope", "String English"));
+    CHECK("Not this one" == moFR.LookupWithContext("TEST|String|1", "Not this one"));
 }
